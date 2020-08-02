1d35c33da4f1c888e2312a9a8767d8b3362948e0
Torra Agustin <torra.agustinignacio@gmail.com>
2020-08-02 20:02:06 -0300

    Modify App component to show PaymentForm component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import Axios from "axios";
+import { PaymentForm } from "./components/payment/PaymentForm";
+import styled from "styled-components"
+
+const Title = styled.h1`
+  color: tomato;
+`;
 
 function App() {
-  const [mediosDePago, setMediosDePago] = useState([]);
+  // const [mediosDePago, setMediosDePago] = useState([]);
 
-  useEffect(() => {
-    Axios.get("/medios")
-      .then((response) => setMediosDePago(response.data))
-      .catch((err) => console.log(err));
-  }, []);
+  // useEffect(() => {
+  //   Axios.get("/medios")
+  //     .then((response) => setMediosDePago(response.data))
+  //     .catch((err) => console.log(err));
+  // }, []);
 
   return (
     <div className="App">
-      {mediosDePago.map((medios) => (
+      <Title>Payments form</Title>
+      <PaymentForm></PaymentForm>
+      {/* {mediosDePago.map((medios) => (
         <p>{medios.name}</p>
-      ))}
+      ))} */}
     </div>
   );
 }
