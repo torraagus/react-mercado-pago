23298a0889e50f5a32413fbc050129f5fa9d8152
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-08 00:06:41 -0300

    Update file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,86 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Axios from "axios";
 import { PaymentForm } from "./components/payment/PaymentForm";
-import styled from "styled-components"
+import styled from "styled-components";
+import { Products } from "./components/products/Products";
 
 const Title = styled.h1`
-  color: tomato;
+	color: #581b98;
+	margin: 2rem 0 0 0;
+`;
+
+const Subtitle = styled.p`
+	color: #f3558e;
+	letter-spacing: 3px;
+	margin: 0;
+	margin-bottom: 2rem;
 `;
 
 function App() {
-  // const [mediosDePago, setMediosDePago] = useState([]);
+	const [paymentVisible, setPaymentVisible] = useState(false);
+	const [processingPayment, setProcessingPayment] = useState(false);
+	const [paymentState, setPaymentState] = useState(null);
+	const [paymentErr, setPaymentErr] = useState(null);
+	const [selectedProduct, setSelectedProduct] = useState(null);
 
-  // useEffect(() => {
-  //   Axios.get("/medios")
-  //     .then((response) => setMediosDePago(response.data))
-  //     .catch((err) => console.log(err));
-  // }, []);
+	const handleOnProductSelected = (product) => {
+		setPaymentVisible(true);
+		setSelectedProduct(() => product);
+	};
 
-  return (
-    <div className="App">
-      <Title>Payments form</Title>
-      <PaymentForm></PaymentForm>
-      {/* {mediosDePago.map((medios) => (
-        <p>{medios.name}</p>
-      ))} */}
-    </div>
-  );
+	const handleOnProcessingPayment = () => {
+		setProcessingPayment(true);
+		setPaymentErr(null);
+		setPaymentState(null);
+	};
+
+	const handleOnPaymentSuccess = (state) => {
+		setPaymentState(state);
+	};
+
+	const handleOnPaymentError = (err) => {
+		setPaymentErr(`${err}`);
+		// setProcessingPayment(false);
+	};
+
+	return (
+		<div className="App">
+			{!processingPayment && (
+				<>
+					<Title>Select a product</Title>
+					<Products onProductSelected={handleOnProductSelected} />
+				</>
+			)}
+			{paymentVisible && !processingPayment && (
+				<>
+					<Title>Pay product</Title>
+					<Subtitle>{selectedProduct?.desc}</Subtitle>
+					<PaymentForm
+						amount={selectedProduct?.price}
+						onProcessingPayment={handleOnProcessingPayment}
+						onPaymentSuccess={handleOnPaymentSuccess}
+						onPaymentError={handleOnPaymentError}
+					></PaymentForm>
+				</>
+			)}
+			{processingPayment && (
+				<div>
+					{!paymentErr && !paymentState && <p>Procesando pago...</p>}
+					{paymentErr && <p>Error: {paymentErr}</p>}
+					{paymentState && <p>Your payment was {paymentState}</p>}
+					{(paymentErr || paymentState) && (
+						<button
+							onClick={() => {
+								window.location.reload(false);
+							}}
+						>
+							Reload!
+						</button>
+					)}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
--- src/components/payment/PaymentForm.js
+++ src/components/payment/PaymentForm.js
@@ -1,24 +1,179 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, Fragment } from "react";
 import Axios from "axios";
+import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
+import styled from "styled-components";
 
-export const PaymentForm = () => {
+const SubmitBtn = styled.input`
+	background-color: #581b98;
+	color: #faee1c;
+	font-weight: bold;
+	outline: none;
+	border: none;
+	text-transform: uppercase;
+	height: 50px;
+	width: 40%;
+	align-self: center;
+	text-indent: 0;
+	border-radius: 15px;
+	letter-spacing: 2px;
+	margin-top: 2rem;
+
+	-webkit-box-shadow: 10px 10px 5px 0px #651a1b31;
+	-moz-box-shadow: 10px 10px 5px 0px #651a1b2f;
+	box-shadow: 5px 5px 5px 0px #651a1b27;
+
+	:hover {
+		cursor: pointer;
+		opacity: 90%;
+	}
+
+	@media (max-width: 512px) {
+		margin-top: 1rem;
+		width: 100%;
+	}
+`;
+
+const Select = styled.select`
+	height: 40px;
+	text-indent: 0.25rem;
+	letter-spacing: 1px;
+`;
+
+const Form = styled.form`
+	width: 40%;
+	display: ${(props) => (props.isVisible ? "none" : "flex")};
+	flex-wrap: wrap;
+	margin: 2rem 0 1rem 0;
+	padding-bottom: 2rem;
+	// justify-content: center;
+	// background-color: tomato;
+
+	@media (max-width: 1366px) {
+		width: 60%;
+	}
+
+	@media (max-width: 1024px) {
+		width: 70%;
+	}
+
+	@media (max-width: 768px) {
+		width: 80%;
+	}
+
+	@media (max-width: 512px) {
+		width: 90%;
+		height: 40vh;
+		overflow-y: auto;
+	}
+`;
+
+const InputWrapper = styled.p`
+	display: flex;
+	flex-direction: column;
+	text-align: left;
+	width: 100%;
+	margin: 0.5rem 0.25rem 0.5rem 0.25rem;
+
+	@media (max-width: 2000px) {
+		:nth-child(1),
+		:nth-child(2) {
+			width: 47%;
+		}
+
+		:nth-child(6),
+		:nth-child(9) {
+			width: 100%;
+		}
+
+		:nth-child(3),
+		:nth-child(4),
+		:nth-child(7) {
+			width: 31%;
+		}
+
+		:nth-child(8) {
+			width: 65%;
+		}
+
+		:nth-child(5) {
+			width: 31%;
+		}
+	}
+
+	@media (max-width: 768px) {
+		width: 100%;
+
+		:nth-child(3),
+		:nth-child(4),
+		:nth-child(7) {
+			width: 31%;
+		}
+
+		:nth-child(8) {
+			width: 65%;
+		}
+
+		:nth-child(5) {
+			width: 31%;
+		}
+	}
+
+	@media (max-width: 512px) {
+		width: 100%;
+
+		:nth-child(n) {
+			width: 100%;
+		}
+	}
+`;
+
+const Input = styled.input`
+	height: 34px;
+	text-indent: 0.5rem;
+	letter-spacing: 1px;
+
+	:focus {
+		outline: none;
+		border: 1px solid #f3558e;
+		border-bottom: 2px solid #9c1de7;
+		border-left: 2px solid #9c1de7;
+		background-color: #9d1de713;
+	}
+`;
+
+const Label = styled.label`
+	letter-spacing: 2px;
+	margin-bottom: 0.5rem;
+`;
+
+export const PaymentForm = ({
+	amount,
+	onProcessingPayment,
+	onPaymentSuccess,
+	onPaymentError,
+}) => {
+	// States
 	const [options, setOptions] = useState([]);
 	const [doSubmit, setDoSubmit] = useState(false);
 	const [cardToken, setCardToken] = useState(null);
 
-	const [transactionAmount, setTransactionAmount] = useState(100);
-	const [cardNumber, setCardNumber] = useState("");
+	const [transactionAmount, setTransactionAmount] = useState(amount);
+	const [cardNumber, setCardNumber] = useState("4509 9535 6623 3704");
 	const [cardHolderName, setCardHolderName] = useState("");
-	const [cardExpirationMonth, setCardExpirationMonth] = useState("");
-	const [cardExpirationYear, setCardExpirationYear] = useState("");
-	const [securityCode, setSecurityCode] = useState("");
+	const [cardExpirationMonth, setCardExpirationMonth] = useState("11");
+	const [cardExpirationYear, setCardExpirationYear] = useState("25");
+	const [securityCode, setSecurityCode] = useState("123");
 	const [docNumber, setDocNumber] = useState("");
 	const [email, setEmail] = useState("");
 	const [paymentMethodId, setPaymentMethodId] = useState("");
 	const [installments, setInstallments] = useState(1);
-	const [description, setDescription] = useState("Some item");
+	const [description, setDescription] = useState("Some product");
+	const [paymentState, setPaymentState] = useState(null);
 
+	// Refs
 	const payFormRef = useRef(null);
+	const focusRef = useRef(null);
 
 	window.Mercadopago.setPublishableKey(
 		"TEST-0dc15ecd-471b-4b8e-9591-7f7a5b8c8acd"
@@ -38,12 +193,25 @@ export const PaymentForm = () => {
 	}, [cardNumber]);
 
 	useEffect(() => {
+		focusRef.current.focus();
+	}, []);
+
+	useEffect(() => {
 		if (cardToken) processPayment();
 	}, [cardToken]);
 
 	useEffect(() => {
 		if (paymentMethodId) getInstallments();
 	}, [paymentMethodId]);
+
+	useEffect(() => {
+		setTransactionAmount(() => amount);
+		if (paymentMethodId) getInstallments();
+	}, [amount]);
+
+	useEffect(() => {
+		if (paymentMethodId) getInstallments();
+	}, [transactionAmount]);
 
 	const setPaymentMethod = (status, response) => {
 		if (status === 200) {
@@ -83,6 +251,7 @@ export const PaymentForm = () => {
 		} else {
 			setCardToken(response.id);
 			setDoSubmit(true);
+			onProcessingPayment();
 		}
 	};
 
@@ -105,42 +274,51 @@ export const PaymentForm = () => {
 		)
 			.then((res) => {
 				console.log(res.data);
+				console.log(res.data.status);
+				console.log(res.data.status_detail);
 				setDoSubmit(false);
+				setPaymentState(res.data.status);
+				onPaymentSuccess(res.data.status);
 			})
 			.catch((err) => {
 				console.log(err);
+				onPaymentError(err);
 				setDoSubmit(false);
 			});
 	};
 
 	return (
-		<form ref={payFormRef} onSubmit={doPay} method="post" id="pay" name="pay">
-			<fieldset>
-				<p>
-					<label htmlFor="description">Descripción</label>
-					<input
-						type="text"
-						name="description"
-						id="description"
-						value={description}
-						onChange={(e) => setDescription(e.target.value)}
+		<>
+			<Cards
+				number={cardNumber}
+				name={cardHolderName}
+				expiry={`${cardExpirationMonth}${cardExpirationYear}`}
+				cvc={securityCode}
+				// focused={focus}
+			/>
+			{paymentState && <p>The payment was {paymentState}</p>}
+			<Form
+				isVisible={doSubmit}
+				ref={payFormRef}
+				onSubmit={doPay}
+				method="post"
+				id="pay"
+				name="pay"
+			>
+				{/* <p>
+					<Label htmlFor="description">Descripción</Label>
+					<Input
+					type="text"
+					name="description"
+					id="description"
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
 					/>
-				</p>
-				<p>
-					<label htmlFor="transaction_amount">Monto a pagar</label>
-					<input
-						name="transaction_amount"
-						id="transaction_amount"
-						type="number"
-						value={transactionAmount}
-						onChange={(e) =>
-							setTransactionAmount(Number.parseInt(e.target.value))
-						}
-					/>
-				</p>
-				<p>
-					<label htmlFor="cardNumber">Número de la tarjeta</label>
-					<input
+				</p> */}
+				<InputWrapper>
+					<Label htmlFor="cardNumber">Número de la tarjeta</Label>
+					<Input
+						ref={focusRef}
 						type="text"
 						id="cardNumber"
 						data-checkout="cardNumber"
@@ -149,20 +327,20 @@ export const PaymentForm = () => {
 						onChange={(e) => setCardNumber(e.target.value)}
 						value={cardNumber}
 					/>
-				</p>
-				<p>
-					<label htmlFor="cardholderName">Nombre y apellido</label>
-					<input
+				</InputWrapper>
+				<InputWrapper>
+					<Label htmlFor="cardholderName">Nombre y apellido</Label>
+					<Input
 						type="text"
 						id="cardholderName"
 						data-checkout="cardholderName"
 						onChange={(e) => setCardHolderName(e.target.value)}
 						value={cardHolderName}
 					/>
-				</p>
-				<p>
-					<label htmlFor="cardExpirationMonth">Mes de vencimiento</label>
-					<input
+				</InputWrapper>
+				<InputWrapper>
+					<Label htmlFor="cardExpirationMonth">Mes de vencimiento</Label>
+					<Input
 						type="text"
 						id="cardExpirationMonth"
 						data-checkout="cardExpirationMonth"
@@ -170,10 +348,10 @@ export const PaymentForm = () => {
 						onChange={(e) => setCardExpirationMonth(e.target.value)}
 						value={cardExpirationMonth}
 					/>
-				</p>
-				<p>
-					<label htmlFor="cardExpirationYear">Año de vencimiento</label>
-					<input
+				</InputWrapper>
+				<InputWrapper>
+					<Label htmlFor="cardExpirationYear">Año de vencimiento</Label>
+					<Input
 						type="text"
 						id="cardExpirationYear"
 						data-checkout="cardExpirationYear"
@@ -181,10 +359,10 @@ export const PaymentForm = () => {
 						onChange={(e) => setCardExpirationYear(e.target.value)}
 						value={cardExpirationYear}
 					/>
-				</p>
-				<p>
-					<label htmlFor="securityCode">Código de seguridad</label>
-					<input
+				</InputWrapper>
+				<InputWrapper>
+					<Label htmlFor="securityCode">Código de seguridad</Label>
+					<Input
 						type="text"
 						id="securityCode"
 						data-checkout="securityCode"
@@ -192,10 +370,22 @@ export const PaymentForm = () => {
 						onChange={(e) => setSecurityCode(e.target.value)}
 						value={securityCode}
 					/>
-				</p>
-				<p>
-					<label htmlFor="installments">Cuotas</label>
-					<select
+				</InputWrapper>
+				{/* <p>
+						<Label htmlFor="transaction_amount">Monto a pagar</Label>
+						<Input
+							name="transaction_amount"
+							id="transaction_amount"
+							type="number"
+							value={transactionAmount}
+							onChange={(e) =>
+								setTransactionAmount(Number.parseInt(e.target.value))
+							}
+						/>
+					</p> */}
+				<InputWrapper>
+					<Label htmlFor="installments">Cuotas</Label>
+					<Select
 						id="installments"
 						className="form-control"
 						name="installments"
@@ -208,25 +398,25 @@ export const PaymentForm = () => {
 									{opt.recommended_message}
 								</option>
 							))}
-					</select>
-				</p>
-				<p>
-					<label htmlFor="docType">Tipo de documento</label>
-					<select id="docType" data-checkout="docType"></select>
-				</p>
-				<p>
-					<label htmlFor="docNumber">Número de documento</label>
-					<input
+					</Select>
+				</InputWrapper>
+				<InputWrapper>
+					<Label htmlFor="docType">Tipo de documento</Label>
+					<Select id="docType" data-checkout="docType"></Select>
+				</InputWrapper>
+				<InputWrapper>
+					<Label htmlFor="docNumber">Número de documento</Label>
+					<Input
 						type="text"
 						id="docNumber"
 						data-checkout="docNumber"
 						onChange={(e) => setDocNumber(e.target.value)}
 						value={docNumber}
 					/>
-				</p>
-				<p>
-					<label htmlFor="email">Email</label>
-					<input
+				</InputWrapper>
+				<InputWrapper>
+					<Label htmlFor="email">Email</Label>
+					<Input
 						type="email"
 						id="email"
 						name="email"
@@ -234,15 +424,17 @@ export const PaymentForm = () => {
 						onChange={(e) => setEmail(e.target.value)}
 						value={email}
 					/>
-				</p>
-				<input
+				</InputWrapper>
+				<Input
 					type="hidden"
 					name="payment_method_id"
 					id="payment_method_id"
 					value={paymentMethodId}
 				/>
-				<input type="submit" value="Pagar" />
-			</fieldset>
-		</form>
+				<InputWrapper>
+					<SubmitBtn type="submit" value="Pagar" />
+				</InputWrapper>
+			</Form>
+		</>
 	);
 };
