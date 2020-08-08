c64c2dce34544e8c91cdefaf702af46f17f499b8
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-08 15:52:57 -0300

    Create file

---
+++ src/components/shared/shared.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+	color: #581b98;
+	margin: 2rem 0 0 0;
+	padding-top: 1rem;
+`;
+
+export const Subtitle = styled.p`
+	color: #f3558e;
+	letter-spacing: 3px;
+	margin: 0;
+	margin-bottom: 2rem;
+`;
