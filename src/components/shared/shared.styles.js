dbf3a94c49b1ba81dcb9097ec01f8e019c6648d4
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-11 18:23:06 -0300

    Create styled component Button

--- src/components/shared/shared.styles.js
+++ src/components/shared/shared.styles.js
@@ -12,3 +12,26 @@ export const Subtitle = styled.p`
 	margin: 0;
 	margin-bottom: 2rem;
 `;
+
+export const Button = styled.button`
+	padding: 1rem 3rem 1rem 3rem;
+	outline: none;
+	border: none;
+	color: #faee1c;
+	font-weight: bold;
+	background-color: #9c1de7;
+	border-radius: 15px;
+
+	-webkit-box-shadow: 10px 10px 5px 0px #651a1b31;
+	-moz-box-shadow: 10px 10px 5px 0px #651a1b2f;
+	box-shadow: 5px 5px 5px 0px #651a1b27;
+
+	:hover {
+		cursor: pointer;
+		opacity: 90%;
+	}
+
+	:active {
+		opacity: 75%;
+	}
+`;
