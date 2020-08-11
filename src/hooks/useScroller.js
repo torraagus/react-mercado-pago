cec89e8f74aeea349e089ab6993c9863ee950e8e
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-11 16:04:43 -0300

    Refactor hook

--- src/hooks/useScroller.js
+++ src/hooks/useScroller.js
@@ -1,11 +1,8 @@
-import React from "react";
-
 export const useScroller = () => {
+
 	const scrollTo = (offset) => {
-		window.scrollTo({
-			top: offset,
-			behavior: "smooth",
-		});
+		window.scrollTo({ top: offset, behavior: "smooth" });
 	};
+
 	return { scrollTo };
 };
